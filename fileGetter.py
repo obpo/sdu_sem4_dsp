5014894621a41d75caa08106cf4549a80720cc49
Oliver Bebe Poulin <oliver@bebepoulin.dk>
2025-06-13 17:20:00 +0200

    Delete fileGetter.py

--- fileGetter.py
+++
@@ -1,9 +0,0 @@
-from zipfile import ZipFile
-
-path = 'data'
-file_name = 'by_field.zip'
-
-if __name__ == '__main__':
-
-    with ZipFile(f'{path}\\{file_name}', 'r') as zip:
-        zip.printdir()
